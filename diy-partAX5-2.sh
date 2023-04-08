2109c87c610865bc6d184f7a3efd1168e26c66e1
xydaozhu <55057386+xydaozhu@users.noreply.github.com>
2023-04-08 14:00:26 +0800

    Update diy-partAX5-2.sh

--- diy-partAX5-2.sh
+++ diy-partAX5-2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=odhcpd  odhcp6c /g' include/target.mk
+# sed -i 's/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=odhcpd  odhcp6c /g' include/target.mk
